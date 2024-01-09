563f2eda10115ef9417b2d941bc6f22a2a4f328b
Arsen <71315641+arersen@users.noreply.github.com>
2024-01-09 10:03:32 +0200

    Update advanced_user_info.py

--- plugins/advanced_user_info.py
+++ plugins/advanced_user_info.py
@@ -4,11 +4,15 @@ from telethon.tl.functions.messages import SendReactionRequest
 import subprocess
 #by awlik 09.01.2024
 class UserInfo:
-  def __init__(who):
+  def __init__(who, get_user=None, username):
+    if get_user:
+      who = who.client(functions.help.GetUserInfoRequest(user_id=username))
+  
     self.first_name = who.first_name
     self.last_name = who.last_name
-    self.id = who.user_id
+    self.id = who.from_id
     self.avatar = who.avatar
+
   
 class AdvancedUserInfo(PluginBase):
     """Andvanced User info by .userinfo command"""
@@ -19,9 +23,17 @@ class AdvancedUserInfo(PluginBase):
         
     async def on_command(self, event, args) -> str:
         if args[0] == "userinfo":
-          if len(args) == 1:
-            output = f"{event.chat.first_name} ({event.chat.username})\n"
-            output += f"{event.chat.id}\n"
-            output += f"{event.chat.phone}"
+          if event.reply_to:
+            message = await self.api.client.get_messages(chat.id, ids=event.reply_to.reply_to_msg_id)
+            user = UserInfo(event, get_user=True, message.from_id)
+          elif len(args) == 1:
+            user = UserInfo(event)
 
-            return output
+          elif len(args) > 1:
+            user = UserInfo(event, get_user=True, args[1])
+          
+            
+          
+
+           
+        return f'Fist name: {user.first_name}\nLast name: {user.last_name}\nID: {user.id}\n'
