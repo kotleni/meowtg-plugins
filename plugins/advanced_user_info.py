fb2130b45c0ec37e2a492df3bd25e2ebf0da1054
Arsen <71315641+arersen@users.noreply.github.com>
2024-01-09 09:49:45 +0200

    Update advanced_user_info.py

--- plugins/advanced_user_info.py
+++ plugins/advanced_user_info.py
@@ -1,3 +1,27 @@
+from plugin_base import PluginBase
+from telethon import TelegramClient, events, types, functions
+from telethon.tl.functions.messages import SendReactionRequest
+import subprocess
+#by awlik 09.01.2024
+class UserInfo:
+  def __init__(who):
+    self.first_name = who.first_name
+    self.last_name = who.last_name
+    self.id = who.user_id
+    self.avatar = who.avatar
+  
+class AdvancedUserInfo(PluginBase):
+    """Andvanced User info by .userinfo command"""
+    enabled = True
 
+    def __init__(self, api) -> None:
+        super().__init__(api)
+        
+    async def on_command(self, event, args) -> str:
+        if args[0] == "userinfo":
+          if len(args) == 1:
+            output = f"{event.chat.first_name} ({event.chat.username})\n"
+            output += f"{event.chat.id}\n"
+            output += f"{event.chat.phone}"
 
-
+            return output
