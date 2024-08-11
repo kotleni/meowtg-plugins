9c178345ee7636dcb4313fdd48d426e4c3221084
Viktor Varenik <yavarenikya@gmail.com>
2024-08-11 22:43:10 +0000

    feat(loli.py): images now is not downloading to cache

--- plugins/loli.py
+++ plugins/loli.py
@@ -7,22 +7,7 @@ from result_codes import *
 
 api_url = "https://api.lolicon.app/setu/v2?size=original&size=regular"
 
-def download_image(image_url, save_path):
-    try:
-        # Send a GET request to the image URL
-        response = requests.get(image_url, stream=True)
-        response.raise_for_status()  # Check if the request was successful
-        
-        # Open a file in binary write mode and save the image content
-        with open(save_path, 'wb') as file:
-            for chunk in response.iter_content(chunk_size=8192):
-                if chunk:
-                    file.write(chunk)
-        print(f"Image successfully downloaded: {save_path}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-def download():
+def get_random_image():
     # Fetch data from the API
     try:
         response = requests.get(api_url)
@@ -37,15 +22,8 @@ def download():
         # Extract the image URL
         image_data = data['data'][0]
         image_url = image_data['urls']['original']
-        image_ext = image_data['ext']
-        image_pid = image_data['pid']
         
-        # Define the path where the image will be saved
-        save_path = f"loli.{image_ext}"
-        
-        # Download the image
-        download_image(image_url, save_path)
-        return save_path
+        return image_url
     except Exception as e:
         print(f"An error occurred while fetching data from the API: {e}")
 
@@ -61,6 +39,6 @@ class Loli(PluginBase):
             chat = await event.get_chat()
             message = event.message
             await self.api.client.edit_message(chat, message, f'.loli **(processing)**')
-            path = download()
+            path = get_random_image()
             await self.api.client.send_file(chat, path, force_document=False)
             return COMMAND_OK_MESSAGE_REMOVE
