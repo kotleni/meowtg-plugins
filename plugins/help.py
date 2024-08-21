3c31a49710e9067234c5e6c7db1727d82116a8d4
Viktor Varenik <yavarenikya@gmail.com>
2024-08-21 22:03:29 +0300

    fix(help): migrate plugin to actual version

--- plugins/help.py
+++ plugins/help.py
@@ -5,19 +5,17 @@ from telethon.tl.functions.messages import SendReactionRequest
 class Help(PluginBase):
     def __init__(self, header, api) -> None:
         super().__init__(header, api)
-    
+
     async def load(self):
         pass
 
     async def on_command(self, event, args) -> str:
         if args[0] == "plugins":
             output = ""
-            for plugin in self.api.plugins_loader.plugins:
+            for plugin in self.api.plugins_loader.get_loaded_plugins():
                 class_name = plugin.__class__.__name__
-                description = plugin.description
-                if plugin.__doc__:
-                    description = plugin.__doc__
+                description = plugin.header.description
 
                 output += f'{class_name} - {description}\n'
-                
+
             return output
