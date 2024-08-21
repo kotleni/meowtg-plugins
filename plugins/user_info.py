c54df4b3ac8c37fc72b576d6b34062f7a497e0f5
Viktor Varenik <yavarenikya@gmail.com>
2024-08-21 22:12:03 +0300

    add user_info plugin

--- plugins/user_info.py
+++ plugins/user_info.py
@@ -2,18 +2,15 @@ from plugin_base import PluginBase
 from telethon import TelegramClient, events, types
 from telethon.tl.functions.messages import SendReactionRequest
 import subprocess
-    
-class UserInfo(PluginBase):
-    """User info by .user command"""
-    enabled = True
 
-    def __init__(self, api) -> None:
-        super().__init__(api)
-        
+class UserInfo(PluginBase):
+    def __init__(self, header, api) -> None:
+        super().__init__(header, api)
+
     async def on_command(self, event, args) -> str:
         if args[0] == "user":
-            output = f"{event.chat.first_name} ({event.chat.username})\n"
-            output += f"{event.chat.id}\n"
-            output += f"{event.chat.phone}"
+            output = f"{event.sender.first_name} ({event.sender.username})\n"
+            output += f"{event.sender.id}\n"
+            output += f"{event.sender.phone}"
 
             return output
