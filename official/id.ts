f326770ea7d3063858d5e0f9261157bd8458007e
Viktor Varenik <yavarenikya@gmail.com>
2025-01-24 15:50:53 +0200

    chore: updated all plugins

--- official/id.ts
+++ official/id.ts
@@ -1,8 +1,7 @@
-import BasePlugin from "../meowtg/base_plugin";
-import PluginsAPI from "../meowtg/plugins_api";
+import BasePlugin from "../meowtg/plugins/base_plugin";
+import PluginsAPI from "../meowtg/plugins/plugins_api";
 import {Api} from "telegram";
 import Message = Api.Message;
-import PeerUser = Api.PeerUser;
 import {getDisplayName} from "telegram/Utils";
 
 export default class IdPlugin implements BasePlugin {
@@ -11,7 +10,7 @@ export default class IdPlugin implements BasePlugin {
     api: PluginsAPI;
 
     async onLoad() {
-        this.api.getCommandsProcessor()
+        await this.api.commandsProcessor
             .register(this.name, this.description, (args: string[], message: Message) => this.onIdCommand(args, message));
     }
 
