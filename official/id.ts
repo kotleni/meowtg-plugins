b11137de6ef6ffcd6c9afad87c7c7f7295caca78
Viktor Varenik <yavarenikya@gmail.com>
2025-06-03 12:34:41 +0300

    feat: update id plugin for new api

--- official/id.ts
+++ official/id.ts
@@ -1,29 +1,28 @@
 import BasePlugin from "../meowtg/plugin/basePlugin";
-import PluginsAPI from "../meowtg/plugin/pluginsApi";
 import {Api} from "telegram";
 import Message = Api.Message;
 import {getDisplayName} from "telegram/Utils";
+import {showResult} from "../meowtg/utils";
 
-export default class IdPlugin implements BasePlugin {
+export default class IdPlugin extends BasePlugin {
     name: string = "id";
     description: string = "Get the id of the user.";
-    api: PluginsAPI;
 
     async onLoad() {
-        await this.api.commandsProcessor
+        await this.commandsProcessor
             .register(this.name, this.description, (args: string[], message: Message) => this.onIdCommand(args, message));
     }
 
     async onUnload() {
-        this.api.commandsProcessor.unregister(this.name);
+        this.commandsProcessor.unregister(this.name);
     }
 
     private async onIdCommand(args: string[], message: Message) {
         if(message.replyTo) { // If reply
             const replyMessage = await message.getReplyMessage();
-            await this.api.showResult(message, `${getDisplayName(replyMessage.sender)} id is ${replyMessage.sender.id}`);
+            await showResult(message, `${getDisplayName(replyMessage.sender)} id is ${replyMessage.sender.id}`);
         } else { // Self
-            await this.api.showResult(message, `Mine id is ${message.sender.id}`);
+            await showResult(message, `Mine id is ${message.sender.id}`);
         }
     }
 }
